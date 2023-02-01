344f906c8239aa882c3f68341ac453e6523831dc
Matt Thurling <mthurling@icloud.com>
2023-02-01 18:59:07 +0000

    bs

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center p-28">
     <h1 class="text-4xl mb-12">
-      Based on your profile, these activities and discussion groups are available:
+      Based on your profile, these activities and discussion groups are available
     </h1>
     <a class="btn btn-lg btn-primary mx-5 mt-12">
       Next
